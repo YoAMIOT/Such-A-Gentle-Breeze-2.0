d1a00ac811cbea9b2452ee329c3d6469d1ae5838
AMIOT Yoann <yoann.amiot.dev@gmail.com>
2022-03-20 12:28:44 +0100

    main text scene script skeleton seems to work

--- scripts/TextScene.cs
+++ scripts/TextScene.cs
@@ -6,7 +6,6 @@ public class TextScene : Control
     private DataManager DataManager;
     private RichTextLabel RTL;
     private Timer TypingTimer;
-    public string currentScene;
     private Godot.Collections.Dictionary currentSceneData;
     private Godot.Collections.Array currentSceneText;
     private string choiceASceneRef;
@@ -21,11 +20,12 @@ public class TextScene : Control
         TypingTimer = GetNode<Timer>("TypingTimer");
 
         if(DataManager.currentScene != ""){
-            gatherAllSceneDatas(currentScene);
+            GD.Print("else");
+            gatherAllSceneDatas(DataManager.currentScene);
         }
         else if(DataManager.currentScene == ""){
             DataManager.currentScene = "1-1";
-            gatherAllSceneDatas(currentScene);
+            gatherAllSceneDatas(DataManager.currentScene);
         }
 
         RTL.BbcodeText = ((string)currentSceneText[page]);
@@ -39,21 +39,38 @@ public class TextScene : Control
     //Input function
     public override void _Input(InputEvent inputEvent){
         if(inputEvent is InputEventMouseButton && inputEvent.IsPressed()){
-            if(RTL.VisibleCharacters > RTL.GetTotalCharacterCount()){
-                return;
+            if(RTL.VisibleCharacters == RTL.GetTotalCharacterCount()){
+                if (page < currentSceneText.Count - 1){
+                    page += 1;
+                    RTL.BbcodeText = (string)currentSceneText[page];
+                    RTL.VisibleCharacters = 0;
+                    TypingTimer.Paused = false;
+                }
+                else if(page == currentSceneText.Count){
+                    GD.Print("page writing done");
+                }
             }
             //Show all the characters
             else {
                 RTL.VisibleCharacters = RTL.GetTotalCharacterCount();
+                TypingTimer.Paused = true;
             }
         }
     }
 
 
-    //TimeOut function
+
+    //Typing timer timeOut function
     private void typingTimerTimeout(){
         if(RTL.VisibleCharacters < RTL.GetTotalCharacterCount()){
             RTL.VisibleCharacters = RTL.VisibleCharacters + 1;
+        }
+        else if(RTL.VisibleCharacters > RTL.GetTotalCharacterCount()){
+            RTL.VisibleCharacters = RTL.GetTotalCharacterCount();
+        }
+
+        if(RTL.VisibleCharacters == RTL.GetTotalCharacterCount()){
+            TypingTimer.Paused = true;
         }
     }
 
@@ -61,7 +78,6 @@ public class TextScene : Control
 
     //Function to get the scene datas and apply them to the script
     private void gatherAllSceneDatas(string sceneRef){
-        currentScene = DataManager.currentScene;
         currentSceneData = DataManager.loadSceneFromJson(sceneRef);
         currentSceneText = (Godot.Collections.Array)currentSceneData["text"];
         choiceASceneRef = (string)currentSceneData["choiceA"];
